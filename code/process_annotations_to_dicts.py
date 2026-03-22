a86b617855f491bba43c38ffd2b2feb5657f96d0
Alyssa Dai <alyssa.ydai@gmail.com>
2026-03-22 02:13:49 -0400

    overwrite log file each time

--- code/process_annotations_to_dicts.py
+++ code/process_annotations_to_dicts.py
@@ -11,6 +11,7 @@ logging.basicConfig(
     format="%(asctime)s %(name)s %(levelname)s: %(message)s",
     datefmt="[%Y-%m-%d %H:%M:%S]",
     filename=Path(__file__).parent / "logs" / f"{Path(__file__).stem}.log",
+    filemode="w",
 )
 logger = logging.getLogger(__name__)
 

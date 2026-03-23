4a309e5ac4eddd840c18105d466f14418ba1a1aa
Alyssa Dai <alyssa.ydai@gmail.com>
2026-03-23 19:15:14 -0400

    simplify script

--- code/count_participants_json_files_with_annotations.py
+++ code/count_participants_json_files_with_annotations.py
@@ -1,6 +1,6 @@
 """
-Identify participants.json files in data/ that contain Neurobagel annotations,
-and if those datasets were previously annotated internally.
+Identify participants.json files in data/ that contain Neurobagel annotations
+and whether those datasets were previously annotated internally.
 """
 
 import json
@@ -11,15 +11,10 @@ import pandas as pd
 ROOT_DIR = Path(__file__).parents[1]
 DATA_DIR = ROOT_DIR / "data"
 RESOURCES_DIR = ROOT_DIR / "resources"
-OUTPUT_PATH = RESOURCES_DIR / "participants_json_files_with_annotations.txt"
 # Overview file with info about which datasets have already been annotated by us
 OPENNEURO_TABULAR_DATASETS_OVERVIEW = RESOURCES_DIR / "openneuro_tabular.tsv"
 
-
-def load_json(path: Path) -> dict:
-    # Use encoding "utf-8-sig" to handle potential BOM in JSON files - common in Windows-created files
-    with open(path, "r", encoding="utf-8-sig") as f:
-        return json.load(f)
+OUTPUT_PATH = RESOURCES_DIR / "participants_json_files_with_annotations.txt"
 
 
 def count_json_files_containing_annotations(
@@ -30,21 +25,22 @@ def count_json_files_containing_annotations(
 
     json_files = list(dir_path.glob("*.json"))
 
-    unparsable = []
+    unparsable_jsons = []
     jsons_with_annotations = []
     for json_file in json_files:
         dataset = json_file.stem
         try:
-            contents = load_json(json_file)
+            with open(json_file, "r", encoding="utf-8-sig") as f:
+                contents = json.load(f)
         except Exception as e:
             print(f"ERROR: Error loading JSON file {json_file}: {e}")
-            unparsable.append(dataset)
+            unparsable_jsons.append(dataset)
             continue
         if not isinstance(contents, dict):
             print(
                 f"WARNING: {json_file} does not contain a dictionary at the top level, skipping."
             )
-            unparsable.append(dataset)
+            unparsable_jsons.append(dataset)
             continue
         for column_contents in contents.values():
             if isinstance(column_contents, dict) and column_contents.get("Annotations"):
@@ -54,15 +50,16 @@ def count_json_files_containing_annotations(
                         "already_annotated_by_us": overview_indexed.loc[dataset],
                     }
                 )
-                break  # No need to check other columns in this file
+                break
 
-    if unparsable:
+    if unparsable_jsons:
         print(
-            f"Unparsable JSON files ({len(unparsable)}/{len(json_files)}): {', '.join(unparsable)}"
+            f"Unparsable JSON files ({len(unparsable_jsons)}/{len(json_files)}): {', '.join(unparsable_jsons)}"
         )
     print(
         f"Number of OpenNeuroDatasets participants.json files in {dir_path} containing 'Annotations': {len(jsons_with_annotations)}/{len(json_files)}"
     )
+
     pd.DataFrame(jsons_with_annotations).to_csv(output_path, index=False, sep="\t")
     print(f"List of dataset IDs with annotations saved to {output_path}")
 
